b04afb6cb69dd73486eedae411b3beaa7079c866
Matheus Iser <matheusiser@squareup.com>
2025-05-29 12:28:57 -0700

    Update SDK version to 2.3.0

--- example/app/build.gradle.kts
+++ example/app/build.gradle.kts
@@ -49,7 +49,7 @@ android {
 dependencies {
   implementation(libs.androidx.fragment.ktx)
   implementation(libs.androidx.appcompat)
-  val squareSdkVersion = "2.2.0"
+  val squareSdkVersion = "2.3.0"
   // Mobile Payments SDK dependency
   implementation("com.squareup.sdk:mobile-payments-sdk:$squareSdkVersion")
 
